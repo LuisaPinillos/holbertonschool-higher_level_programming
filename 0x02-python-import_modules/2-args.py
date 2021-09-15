11103937396b6f7cfc29d79dfe9914bc825dae5d
LuisaPinillos <luisapinillos20@gmail.com>
2021-09-15 13:31:13 -0500

    print the arguments with their number of the position

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == '__main__':
+    lengarvs = len(argv) - 1
+    j = 1
+    if lengarvs == 1 and lengarvs != 0:
+        print("{} argument:" .format(lengarvs))
+        print("{}: {} " .format(lengarvs, argv[1]))
+    elif lengarvs == 0:
+        print("0 arguments." .format(lengarvs))
+    else:
+        print("{} arguments:" .format(lengarvs))
+        for j in range(1, lengarvs + 1):
+            print("{}: {}" .format(j, argv[j]))
