0c79adb9f873840dc33eb3f5c14d57d29e1a04da
LuisaPinillos <luisapinillos20@gmail.com>
2021-09-27 20:34:19 -0500

    function that prints x elements of a list.

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-from typing import MutableMapping
-
-
 def safe_print_list(my_list=[], x=0):
     leng = 0
     while leng < x:
         try:
-            print("{}" .format(my_list[leng]), end="")
+            print("{}".format(my_list[leng]), end="")
             leng += 1
         except IndexError:
             break
