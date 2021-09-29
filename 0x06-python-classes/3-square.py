1f42c5f4cc26fd0c271fa9d5104809e6270eb658
LuisaPinillos <luisapinillos20@gmail.com>
2021-09-28 23:08:03 -0500

    Area of a square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Class Square"""
+
+
+class Square:
+    """Square has a size"""
+
+    def __init__(self, size=0):
+        """This class raise error"""
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return self.__size ** 2
