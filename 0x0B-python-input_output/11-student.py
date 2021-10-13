e995b4dabe86b663eb797748657844cf16ee2aaf
LuisaPinillos <luisapinillos20@gmail.com>
2021-10-13 00:31:23 -0500

    Public method that replaces all attributes of the Student instance

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Write a class Student that defines
+a student by and instantiation with
+first_name, last_name and age
+"""
+
+
+class Student():
+    """
+    Create the class Student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Public method that retrieves a dictionary
+        representation of a Student instance
+        """
+        new_dic = {}
+        if type(attrs) is list:
+            for i in attrs:
+                if type(i) is str and hasattr(self, i):
+                    new_dic[i] = getattr(self, i)
+                else:
+                    pass
+        else:
+            new_dic = self.__dict__
+        return new_dic
+
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            setattr(self, key, value)
