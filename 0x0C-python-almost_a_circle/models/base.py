e4150526000e57f2b0e5dae731f012ebc4dbc6bb
LuisaPinillos <luisapinillos20@gmail.com>
2021-10-18 17:45:43 -0500

    models

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+
+"""
+
+
+class Base():
+    """The Bass class"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+"""
+The class Rectangle that inherits from Base
+Private instance attributes, each with its
+own public getter and setter
+"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Rectangle class inherits Base"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """return self"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """to set it"""
+        self.__width = value
+
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+
+    @property
+    def height(self):
+        """to retrieve it"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """to set it"""
+        self.__height = value
+
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+
+    @property
+    def x(self):
+        """to retrieve it"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """to set it"""
+        self.__x = value
+
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+
+    @property
+    def y(self):
+        """to retrieve it"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """to set it"""
+        self.__y = value
+
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
