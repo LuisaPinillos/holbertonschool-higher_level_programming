972924f5c34d30d12a10843fcf8d7ae769366ae1
LuisaPinillos <luisapinillos20@gmail.com>
2021-11-16 22:41:54 -0500

    Fix the script that creates a table second_table in the database

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -4,9 +4,10 @@ CREATE TABLE IF NOT EXISTS second_table (
     name VARCHAR(256),
     score INT
 );
-INSERT INTO `second_table` (`id`,`name`,`score`)
+INSERT INTO second_table 
+    (id, name, score) 
 VALUES 
-("1", "jhon", "10"),
-("2", "Alex", "3"),
-("3", "Bob", "14"),
-("4", "George", "8")
+    ("1", "jhon", "10"),
+    ("2", "Alex", "3"),
+    ("3", "Bob", "14"),
+    ("4", "George", "8");
