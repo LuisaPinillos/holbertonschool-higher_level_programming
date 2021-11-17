88456ecbf1e7d5c061f746f3e3a598a0bc918682
LuisaPinillos <luisapinillos20@gmail.com>
2021-11-16 23:43:37 -0500

    Fix the script that creates a table and insert the values

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
 -- Script that lists all records of the table second_table of the database
 -- In order of the score
-SELECT name, score FROM second_table ORDER BY score DESC;
+SELECT score, name FROM second_table ORDER BY score DESC;
