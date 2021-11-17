03b2fa8931a7f31cf1ba487651f8046d3a8097fe
LuisaPinillos <luisapinillos20@gmail.com>
2021-11-17 00:02:21 -0500

    Script that removes all records with a score less than or equal 5

--- 0x0D-SQL_introduction/13-change_class.sql
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -1,3 +1,3 @@
--- Script that removes all records with a score 
+-- Script that delete all records with a score 
 -- <= 5 in the table second_table of the database.
 DELETE FROM second_table WHERE score <= 5;
