9b1000524f2da6db04ba8cdba4c49e9988bd121b
LuisaPinillos <luisapinillos20@gmail.com>
2021-11-16 23:08:48 -0500

    Fix the script that creates a table second_table in the database

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,14 +1,11 @@
 -- Script that creates a table second_table in the database hbtn_0c_0
 CREATE TABLE IF NOT EXISTS second_table (
-    id INT DEFAULT 0,
-    name VARCHAR(256) DEFAULT NULL,
-    score INT DEFAULT 0
+    id INT,
+    name VARCHAR(256),
+    score INT
 );
 
-INSERT INTO second_table 
-    (`id`,`name`,`score`)
-VALUES 
-    ("1", "jhon", "10"),
-    ("2", "Alex", "3"),
-    ("3", "Bob", "14"),
-    ("4", "George", "8")
+INSERT INTO second_table(id, name, score) VALUES (1, "jhon", 10);
+INSERT INTO second_table(id, name, score) VALUES (2, "Alex", 3);
+INSERT INTO second_table(id, name, score) VALUES (3, "Bob", 14);
+INSERT INTO second_table(id, name, score) VALUES (4, "George", 8);
