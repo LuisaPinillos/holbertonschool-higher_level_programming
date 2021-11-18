b862a58458c4cc17262f3dcfc8c0ef8f8e116c4d
LuisaPinillos <luisapinillos20@gmail.com>
2021-11-18 12:57:10 -0500

    Fix Script that lists the number of records with the same score

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,9 @@
 -- Script that lists the number of records with the 
 -- same score in the table second_table
-SELECT score,count(score) AS 'number' FROM second_table GROUP BY score HAVING count(score)>1;
+SELECT
+    score,
+    count(score) as number
+FROM
+    second_table
+GROUP BY
+    score
