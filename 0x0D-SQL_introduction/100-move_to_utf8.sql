cc9d7f60b7c86a8c7c0298b249ce9baf260998c9
LuisaPinillos <luisapinillos20@gmail.com>
2021-11-18 14:37:27 -0500

    Script that converts hbtn_0c_0 database to UTF8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,2 @@
 -- Script that converts hbtn_0c_0 database to UTF8
-ALTER TABLE
-    first_table
-    CONVERT TO CHARACTER SET utf8mb4
-    COLLATE utf8mb4_unicode_ci;
+USE hbtn_0c_0 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
