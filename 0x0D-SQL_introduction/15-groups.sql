78d9e4c40aaae6b218905f2195969ccb688b2691
LuisaPinillos <luisapinillos20@gmail.com>
2021-11-18 12:35:05 -0500

    lists the number of records with the same score

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,6 @@
+-- Script that lists the number of records with the 
+-- same score in the table second_table
+SELECT score, COUNT(1) AS number
+FROM second_table
+GROUP BY score
+HAVING COUNT(1) > 1
