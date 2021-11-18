fc5542ed54d8933cc9bb7339b07291191ae38651
LuisaPinillos <luisapinillos20@gmail.com>
2021-11-18 13:51:27 -0500

    Script lists all records of the table

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
 -- Script that lists all records of the table second_table
 -- of the database hbtn_0c_0.
-SELECT score, name FROM second_table ORDER BY score DESC
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
