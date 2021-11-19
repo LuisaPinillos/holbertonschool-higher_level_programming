9f345a7aafa5ba06466ec5ee3eb6dc758dd28831
LuisaPinillos <luisapinillos20@gmail.com>
2021-11-18 23:32:01 -0500

    Fix script

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,6 +1,6 @@
 -- Creates the database hbtn_0d_usa and the table cities
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+CREATE TABLE IF NOT EXISTS cities.hbtn_0d_usa (
     id INT PRIMARY KEY UNIQUE AUTO_INCREMENT NOT NULL,
     state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
