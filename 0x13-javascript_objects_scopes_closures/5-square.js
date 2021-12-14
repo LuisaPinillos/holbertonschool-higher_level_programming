dc7a65aaf6388f29bdb8a503361f4a26c7df8f91
LuisaPinillos <luisapinillos20@gmail.com>
2021-12-14 18:49:29 -0500

    Class square inherits from Rectangle class.

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
+/* Class Square that defines a square and inherits from Rectangle. */
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+    this.size = size;
+  }
+}
+module.exports = Square;
