ea178e428ec3d46a794b8b7fb020c10fd1cbfdd2
LuisaPinillos <luisapinillos20@gmail.com>
2021-12-13 21:31:11 -0500

    prints 3 lines with a loop.

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+// prints 3 lines with a loop.
+
+let i = 0;
+const printlines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+while (i < printlines.length) {
+  console.log(printlines[i]);
+  i++;
+}
