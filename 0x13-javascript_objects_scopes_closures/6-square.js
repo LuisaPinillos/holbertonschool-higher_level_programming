d61e6670304bb02943e214353e3cc56f38289977
LuisaPinillos <luisapinillos20@gmail.com>
2021-12-14 21:45:09 -0500

    fix semistandard.

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-/* Instance method called charPrint(c) that prints the rectangle 
+/* Instance method called charPrint(c) that prints the rectangle
 using the character c */
 
 class Square extends require('./5-square') {
@@ -8,7 +8,7 @@ class Square extends require('./5-square') {
     super(size, size);
   }
 
-  charPrint(c) {
+  charPrint (c) {
     c = c || 'X';
     let string1 = '';
     for (let i = 0; i < this.height; i++) {
@@ -18,7 +18,7 @@ class Square extends require('./5-square') {
       if (i + 1 !== this.height) {
         string1 += '\n';
       }
-      }
+    }
     console.log(string1);
   }
 }
