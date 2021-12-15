c4c5e19c1bcdb7c7627f08d0b0654baee7bf6ddb
LuisaPinillos <luisapinillos20@gmail.com>
2021-12-15 16:20:58 -0500

    Script that computes and prints a factorial.

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+// Script that computes and prints a factorial.
+
+if (parseInt(process.argv[2])) {
+    let fact = 1;
+    for (i = 1; i <= parseInt(process.argv[2]); i++) {
+        fact *= i;
+    }
+    console.log(fact)
+} else {
+    console.log(1)
+}
