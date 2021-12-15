66f53468a095f276d5fb9c93005bb4c8a81d4011
LuisaPinillos <luisapinillos20@gmail.com>
2021-12-15 16:33:44 -0500

    fix code

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
 
 // Script that computes and prints a factorial.
-const { argv } = require('process');
 
-if (parseInt(process.argv[2])) {
-  let fact = 1;
-  for (let i = 1; i <= parseInt(process.argv[2]); i++) {
-    fact *= i;
-  }
-  console.log(fact);
-} else {
-  console.log('1');
+function factorial(a) {
+    let fact = 1;
+    for (let i = 1; i <= a; i++) {
+        fact *= i;
+    }
+    console.log(fact);
 }
+factorial(parseInt(process.argv[2]));
