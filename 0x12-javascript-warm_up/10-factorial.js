c669667890e0f2211cfa0cfbee92f8b6e02c0baa
LuisaPinillos <luisapinillos20@gmail.com>
2021-12-15 16:22:30 -0500

    Seminstandard fix

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,11 +3,11 @@
 // Script that computes and prints a factorial.
 
 if (parseInt(process.argv[2])) {
-    let fact = 1;
-    for (i = 1; i <= parseInt(process.argv[2]); i++) {
-        fact *= i;
-    }
-    console.log(fact)
+  let fact = 1;
+  for (let i = 1; i <= parseInt(process.argv[2]); i++) {
+    fact *= i;
+  }
+  console.log(fact);
 } else {
-    console.log(1)
+  console.log(1);
 }
