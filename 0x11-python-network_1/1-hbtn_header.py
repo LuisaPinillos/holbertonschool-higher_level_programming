cf10ec6ce1e541a7ca4ed34c8713c38997f97917
LuisaPinillos <luisapinillos20@gmail.com>
2022-01-13 19:28:22 -0500

    Script that sends a request to the URL and displays the value of the X-Request-Id.

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Script that sends a request to the URL and displays the value of the X-Request-Id.
+"""
+
+import urllib.request
+from sys import argv
+
+if __name__ == "__main__":
+    url = argv[1]
+    with urllib.request.urlopen(url) as response:
+        html = response.headers
+        print(html["X-Request-Id"])
