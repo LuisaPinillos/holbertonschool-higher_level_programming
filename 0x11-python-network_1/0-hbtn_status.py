c3509279b2e3e387df6836d6300eed1f57543d4c
LuisaPinillos <luisapinillos20@gmail.com>
2022-01-13 19:09:00 -0500

    fix code

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
-# Write a Python script that fetches.
+"""
+Write a Python script that fetches https://intranet.hbtn.io/status
+"""
+
 import urllib.request
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-   html = response.read()
+
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print("Body response:")
+        print("/t- type: {}".format(type(html)))
+        print("/t- content: {}".format(html))
+        print("/t- utf8 content: {}".format(html.decode('utf8')))
