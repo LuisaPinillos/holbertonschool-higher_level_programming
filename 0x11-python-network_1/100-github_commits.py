d47b2634aa9944db3a811ae1f865f34ea4c00486
LuisaPinillos <luisapinillos20@gmail.com>
2022-01-13 22:15:50 -0500

    Fix code

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -11,8 +11,8 @@ from sys import argv
 if __name__ == "__main__":
     rep = argv[1]
     user = argv[2]
-    url = "https://api.github.com/repos/{}/{}".format(user, rep)
-    url += 'commits?per_page=10'
+    url = 'https://api.github.com/repos/{}/{}'.format(user, rep)
+    url += '/commits?per_page=10'
     req = requests.get(url)
     for i in req.json():
         print("{}: {}".format(i.get("sha"
