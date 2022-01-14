427f3c253e5efa0ba8ecf1ae3c098b6c465f8faf
LuisaPinillos <luisapinillos20@gmail.com>
2022-01-13 19:29:40 -0500

    Fix pep8

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Script that sends a request to the URL and displays the value of the X-Request-Id.
+Script that sends a request to the URL and
+displays the value of the X-Request-Id.
 """
 
 import urllib.request
