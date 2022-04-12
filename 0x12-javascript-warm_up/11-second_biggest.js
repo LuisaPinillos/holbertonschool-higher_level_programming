4d101a439b216b7287191a72718ab594a5ed7f06
LuisaPinillos <luisapinillos20@gmail.com>
2022-04-12 11:04:32 -0500

    Fix semistandard

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,13 @@
 #!/usr/bin/node
 
 // Script that searches the second biggest integer in the list of arguments.
-const list_Args = process.argv.length;
-if (list_Args >= 3) {
-  const list_order = process.argv.sort();
-  const sec_max = list_order.reverse()[1];
-  console.log(sec_max);
+
+const listArgs = process.argv.length;
+
+if (listArgs >= 3) {
+  const listOrder = process.argv.sort();
+  const secMax = listOrder.reverse()[1];
+  console.log(secMax);
 } else {
   console.log(0);
 }
