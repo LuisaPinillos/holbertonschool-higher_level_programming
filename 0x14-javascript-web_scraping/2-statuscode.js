4c4c760cfa5b4db487da3ed7bf27c79e11a9ac04
LuisaPinillos <luisapinillos20@gmail.com>
2022-04-12 17:57:09 -0500

    script that display the status code of a GET request.

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,13 +2,13 @@
 
 // script that display the status code of a GET request.
 
-var myUrl = process.argv[2];
+const myUrl = process.argv[2];
 const req = require('request');
 
-req (myUrl, function (error, response) {
-    if (error) {
-     console.log(error);
-    } else {
-     console.log('code:', response.statusCode);
-    }
-   });
+req(myUrl, function (error, response) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('code:', response.statusCode);
+  }
+});
