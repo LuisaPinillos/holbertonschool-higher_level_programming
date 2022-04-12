8e7591ac6adeb3dc8c4bc858dc0730fbaa4c5cc1
LuisaPinillos <luisapinillos20@gmail.com>
2022-04-12 18:52:40 -0500

    Script that prints the title of a Star Wars.

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -7,10 +7,10 @@ const episode = process.argv[2];
 const url = 'https://swapi-api.hbtn.io/api/films/' + episode;
 const req = require('request');
 
-req (url, function (error, response, body) {
-    if (error) {
-     console.log(error);
-    } else {
-     console.log(JSON.parse(body).title);
-    }
-   });
+req(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
